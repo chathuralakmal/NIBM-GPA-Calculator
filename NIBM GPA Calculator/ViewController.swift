d3d38463398278747301348a1271eb1860335dd4
Chathura Lakmal <Mac@Chathuras-MacBook-Pro.local>
2015-08-23 02:25:06 +0530

    Initial Commit

---
+++ NIBM GPA Calculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NIBM GPA Calculator
+//
+//  Created by Chathura Lakmal on 8/23/15.
+//  Copyright (c) 2015 Chathura Lakmal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
